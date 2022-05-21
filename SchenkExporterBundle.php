ac0aba060abae70d17d24ebda6f140638d4556c9
leonline <leonline@theleders.family>
2022-05-21 23:51:37 +0200

    Initial commit

---
+++ SchenkExporterBundle.php
@@ -0,0 +1,9 @@
+namespace KimaiPlugin\SchenkExporterBundle;
+
+use App\Plugin\PluginInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SchenkExporterBundle extends Bundle implements PluginInterface
+{
+}
